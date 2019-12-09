2479353f88098d9c5e6ba61dfcddc27740c28406
rasouus <rasouus@gmail.com>
2019-12-09 15:37:42 +0400

    initialized

---
+++ calculator/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hellllllllllllllllllo");
+            Console.Read();
+        }
+    }
+}
